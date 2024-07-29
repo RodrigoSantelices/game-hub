b9c1f5c7326c0f349ffa42d3b32bdc614b4f4571
Rodrigo Santelices <rcsantelices@gmail.com>
2024-07-29 17:58:58 -0500

    Build Sort Selector

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -26,9 +26,6 @@ const PlatformSelector = ({ onSelectPlatform, selectedPlatform }: Props) => {
             {platform.name}
           </MenuItem>
         ))}
-
-        <MenuItem>Item 2</MenuItem>
-        <MenuItem>Item 3</MenuItem>
       </MenuList>
     </Menu>
   );
