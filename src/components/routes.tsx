810331148d500007a7575e9b115fac6d32e41ba8
Rodrigo Santelices <rcsantelices@gmail.com>
2024-08-28 18:53:55 -0500

    Added Routing and Error Handling

---
+++ src/components/routes.tsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "../pages/Layout.tsx";
+import GameDetailPage from "./GameDetailPage.tsx";
+import HomePage from "../pages/HomePage.tsx";
+import ErrorPage from "../pages/ErrorPage.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "games/:id", element: <GameDetailPage /> },
+    ],
+  },
+]);
+
+export default router;
